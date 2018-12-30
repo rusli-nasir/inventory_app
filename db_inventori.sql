bf7e79d8f1fd8d1f55dbb4aca0b90bb7fdd0c2bc
DESKTOP-2V1ESQP\Dexter <rusli.nasir.m@gmail.com>
2018-12-31 00:48:55 +0800

    koreksi dump sql

--- db_inventori.sql
+++ db_inventori.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50611
  File Encoding         : 65001
 
- Date: 30/12/2018 18:45:24
+ Date: 30/12/2018 22:13:25
 */
 
 SET NAMES utf8mb4;
@@ -66,9 +66,7 @@ CREATE TABLE `tabel_inventori_komputer`  (
   `status` varchar(30) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   PRIMARY KEY (`kd_komputer`) USING BTREE,
   INDEX `kd_lab`(`kd_lab`) USING BTREE,
-  INDEX `id_user`(`id_user`) USING BTREE,
-  CONSTRAINT `tabel_inventori_komputer_ibfk_1` FOREIGN KEY (`kd_lab`) REFERENCES `tabel_laboratorium` (`kd_lab`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `tabel_inventori_komputer_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `tabel_admin` (`id_user`) ON DELETE RESTRICT ON UPDATE RESTRICT
+  INDEX `id_user`(`id_user`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
@@ -82,6 +80,8 @@ INSERT INTO `tabel_inventori_komputer` VALUES ('KOMLAB005', 'LABKOM001', 'PGW180
 INSERT INTO `tabel_inventori_komputer` VALUES ('KOMLAB006', 'LABKOM002', 'PGW18002', 'PC 2', '2018', 'Win 10', 'baik', 'baik', 'baik', 'baik', 'baik', 'baik', 'baik', '-', 'Lengkap', 'Dapat Dipakai');
 INSERT INTO `tabel_inventori_komputer` VALUES ('KOMLAB007', 'LABKOM001', 'PGW18002', 'PC 5', '2018', 'Windows 10', 'buruk', 'baik', 'baik', 'baik', 'baik', 'baik', 'baik', '-', 'Lengkap', 'Dapat Dipakai');
 INSERT INTO `tabel_inventori_komputer` VALUES ('KOMLAB008', 'LABKOM001', 'PGW18002', 'PC 6', '2018', 'Windows 10', 'baik', 'baik', 'baik', 'baik', 'baik', 'baik', 'baik', '-', 'Lengkap', 'Dapat Dipakai');
+INSERT INTO `tabel_inventori_komputer` VALUES ('LABKOM001/DKOM001', 'LABKOM001', 'PGW18003', 'sds', '2323', 'da', 'baik', 'baik', 'baik', 'baik', 'baik', 'baik', 'baik', 'ad', 'sad', 'Dapat Dipakai');
+INSERT INTO `tabel_inventori_komputer` VALUES ('LABKOM001/DKOM002', 'LABKOM001', 'PGW18003', 'asd', '232', 'asd', 'baik', 'baik', 'baik', 'baik', 'baik', 'baik', 'baik', 'asd', 'sad', 'Dapat Dipakai');
 INSERT INTO `tabel_inventori_komputer` VALUES ('LABKOM001001', 'LABKOM001', 'PGW18002', 'sds', '123', 'sdas', 'baik', 'baik', 'baik', 'baik', 'baik', 'baik', 'baik', 'da', 'asd', 'Dapat Dipakai');
 
 -- ----------------------------
@@ -99,9 +99,7 @@ CREATE TABLE `tabel_inventori_non_komputer`  (
   `status` varchar(30) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   PRIMARY KEY (`kd_inventori`) USING BTREE,
   INDEX `kd_lab`(`kd_lab`) USING BTREE,
-  INDEX `id_user`(`id_user`) USING BTREE,
-  CONSTRAINT `tabel_inventori_non_komputer_ibfk_1` FOREIGN KEY (`kd_lab`) REFERENCES `tabel_laboratorium` (`kd_lab`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `tabel_inventori_non_komputer_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `tabel_admin` (`id_user`) ON DELETE RESTRICT ON UPDATE RESTRICT
+  INDEX `id_user`(`id_user`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
@@ -123,8 +121,7 @@ CREATE TABLE `tabel_laboratorium`  (
   `nama_lab` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `status` varchar(30) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   PRIMARY KEY (`kd_lab`) USING BTREE,
-  INDEX `id_user`(`id_user`) USING BTREE,
-  CONSTRAINT `tabel_laboratorium_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `tabel_admin` (`id_user`) ON DELETE RESTRICT ON UPDATE RESTRICT
+  INDEX `id_user`(`id_user`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
@@ -153,9 +150,7 @@ CREATE TABLE `tabel_maintenance`  (
   INDEX `kd_inventori`(`kd_inventori`) USING BTREE,
   INDEX `kd_komputer`(`kd_komputer`) USING BTREE,
   INDEX `kd_lab`(`kd_lab`) USING BTREE,
-  INDEX `id_user`(`id_user`) USING BTREE,
-  CONSTRAINT `tabel_maintenance_ibfk_3` FOREIGN KEY (`kd_lab`) REFERENCES `tabel_laboratorium` (`kd_lab`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `tabel_maintenance_ibfk_4` FOREIGN KEY (`id_user`) REFERENCES `tabel_admin` (`id_user`) ON DELETE RESTRICT ON UPDATE RESTRICT
+  INDEX `id_user`(`id_user`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
@@ -168,5 +163,13 @@ INSERT INTO `tabel_maintenance` VALUES ('M05', '', 'KOMLAB001', 'LABKOM001', 'PG
 INSERT INTO `tabel_maintenance` VALUES ('M06', '', 'KOMLAB004', 'LABKOM001', 'PGW18002', '2018-12-20', '2018-12-22', '2019-01-05', 'Monitor rusak', 'Dikerjakan');
 INSERT INTO `tabel_maintenance` VALUES ('M07', 'NONKOM004', '', 'LABKOM002', 'PGW18002', '2018-12-20', '2018-12-22', '2019-01-05', 'Proyektor rusak', 'Belum Dikerjakan');
 INSERT INTO `tabel_maintenance` VALUES ('M08', 'LABKOM001001', 'KOMLAB001', 'LABKOM001', 'PGW18003', '2018-12-10', '2018-12-10', '2018-12-24', 'dasd', 'Dikerjakan');
+
+ALTER TABLE `tabel_inventori_komputer` ADD CONSTRAINT `tabel_inventori_komputer_ibfk_1` FOREIGN KEY (`kd_lab`) REFERENCES `tabel_laboratorium` (`kd_lab`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE `tabel_inventori_komputer` ADD CONSTRAINT `tabel_inventori_komputer_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `tabel_admin` (`id_user`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE `tabel_inventori_non_komputer` ADD CONSTRAINT `tabel_inventori_non_komputer_ibfk_1` FOREIGN KEY (`kd_lab`) REFERENCES `tabel_laboratorium` (`kd_lab`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE `tabel_inventori_non_komputer` ADD CONSTRAINT `tabel_inventori_non_komputer_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `tabel_admin` (`id_user`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE `tabel_laboratorium` ADD CONSTRAINT `tabel_laboratorium_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `tabel_admin` (`id_user`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE `tabel_maintenance` ADD CONSTRAINT `tabel_maintenance_ibfk_3` FOREIGN KEY (`kd_lab`) REFERENCES `tabel_laboratorium` (`kd_lab`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE `tabel_maintenance` ADD CONSTRAINT `tabel_maintenance_ibfk_4` FOREIGN KEY (`id_user`) REFERENCES `tabel_admin` (`id_user`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 SET FOREIGN_KEY_CHECKS = 1;
